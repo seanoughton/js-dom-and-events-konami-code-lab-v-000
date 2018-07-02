01ab4c08f5c7d364ae84d4783aae5ce63b16eb36
Sean Oughton <seanoughton@gmail.com>
2018-07-02 17:29:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
+  const key = parseInt(e.detail || e.which || e.location);
+
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      console.log('Awesome!')
+    }
+  } else {
+    index = 0;
+  };
 
 }
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', init(e));
