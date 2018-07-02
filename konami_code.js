8ad8c450b4525af545c7e46d902b8f30fffcfab1
Sean Oughton <seanoughton@gmail.com>
2018-07-02 18:30:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-const input = document.body;
-let index = 0;
 
 function init(e){
-  const key = parseInt(e.which || e.detail);
-  if (key === code[index]) {
-    index++;
-    if (index === code.length) {
-      alert('Awesome!')
-    }
-  } else {
-    index = 0;
-    console.log(`Not correct ${key}`)
-  };
-}
+  const input = document.body;
+  let index = 0;
+  input.addEventListener('keydown', function (e) {
+    const key = parseInt(e.which || e.detail);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('Awesome!')
+      }
+    } else {
+      index = 0;
+    };
+  })
 
-input.addEventListener('keydown', init);
+};
