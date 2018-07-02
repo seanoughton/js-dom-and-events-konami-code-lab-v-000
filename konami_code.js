b48c46d02f2292a477d47f6985283c8b59009ca2
Sean Oughton <seanoughton@gmail.com>
2018-07-02 18:14:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,8 +7,8 @@ let index = 0;
 
 input.addEventListener('keydown', init);
 
-function init(e){
-  const key = parseInt(e.which);
+function init(event){
+  const key = parseInt(event.which);
   if (key === code[index]) {
     index++;
     if (index === code.length) {
